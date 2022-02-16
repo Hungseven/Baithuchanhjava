2684aa8c05a660dfed6f9083209c1b04fa323f7e
luan25092000 <nguyenhuuluan17@gmail.com>
2022-02-16 21:45:25 +0700

    commit note

---
+++ BaiThucHanh1602/BaiThucHanh1602/src/App51.java
@@ -0,0 +1,15 @@
+package vong_lap_while;
+ 
+public class HienThiSoChan {
+ 
+    public static void main(String[] args) {
+        int number = 2;
+         
+        System.out.println("Các số chẵn nhỏ hơn hoặc bằng 20 là: ");
+        while (number <= 20) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+ 
+}
